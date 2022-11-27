a02c0411947bfb7e2a6ee3e6bf9e1ab81cc84034
Hocine <ocineh@hotmail.com>
2022-11-27 03:21:15 +0100

    Add the GameModel class

---
+++ src/main/java/project/models/GameModel.java
@@ -0,0 +1,42 @@
+package project.models;
+
+import java.util.Iterator;
+
+public class GameModel extends Model {
+	private final RandomWordList words;
+	private final Stats stats;
+	private int lives, score, level;
+
+	public GameModel(
+			int lives,
+			int score,
+			int level,
+			int nbWords
+	) {
+		this.lives = lives;
+		this.score = score;
+		this.level = level;
+		this.stats = new Stats();
+		this.words = new RandomWordList(nbWords);
+	}
+
+	public int getLives() {
+		return lives;
+	}
+
+	public int getScore() {
+		return score;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public Iterator<String> getWords() {
+		return words.iterator();
+	}
+
+	public String getCurrentWord() {
+		return words.getCurrentWord();
+	}
+}
