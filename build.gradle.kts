f5dba42b4961c40fd4e957e593684a158314cf4a
Hocine <ocineh@hotmail.com>
2023-01-08 11:44:00 +0100

    Rework the tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,13 +12,22 @@ repositories {
 }
 
 dependencies {
-    implementation( group = "org.fxmisc.richtext", name = "richtextfx", version = "0.11.0")
+	implementation(
+		group = "org.fxmisc.richtext",
+		name = "richtextfx",
+		version = "0.11.0"
+	)
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 }
 
 javafx {
 	version = "17"
 	modules = listOf("javafx.controls", "javafx.fxml")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
 }
 
 application.mainClass.set("project.Main")
--- src/test/java/project/models/game/network/NetworkTest.java
+++ src/test/java/project/models/game/network/NetworkTest.java
@@ -3,6 +3,7 @@ package project.models.game.network;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import project.controllers.MenuController;
 import project.controllers.NetworkController;
 import project.models.game.words.Word;
@@ -10,71 +11,77 @@ import project.models.menu.MenuModel;
 
 import java.io.IOException;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class NetworkTest {
-	@BeforeAll public static void setup()
-	throws IOException, InterruptedException {
-		NetworkController.getInstance().host(3333);
+	private static final NetworkController network =
+			NetworkController.getInstance();
+	private static final NetworkModel model = network.getModel();
+
+	@BeforeAll public static void setup() {
+		try {
+			network.host(3333);
+		} catch(Throwable ignored) {}
 	}
 
 	@AfterAll public static void teardown()
 	throws IOException, InterruptedException {
-		NetworkController.getInstance().stop();
+		network.stop();
 	}
 
 	@Test public void host() {
-		if(!NetworkController.getInstance().isRunning())
+		if(!network.isRunning())
 			throw new AssertionError("Network is not running");
 	}
 
 	@Test public void send() throws IOException {
-		NetworkController.getInstance()
-						 .getModel()
-						 .send(Word.normal("test"));
+		network
+				.getModel()
+				.send(Word.normal("test"));
 	}
 
 	@Test public void tryReceiveWord() {
-		if(NetworkController.getInstance()
-							.getModel()
-							.tryReceiveWord() != null)
+		if(network
+				.getModel()
+				.tryReceiveWord() != null)
 			throw new AssertionError("Received word");
 	}
 
 	@Test public void getPlayersList() throws IOException {
-		if(NetworkController.getInstance()
-							.getModel()
-							.getPlayersList()
-							.size() != 1)
+		if(network
+				.getModel()
+				.getPlayersList()
+				.size() != 1)
 			throw new AssertionError("Received players list");
 	}
 
 	@Test public void getServerAddress() {
-		if(NetworkController.getInstance()
-							.getModel()
-							.getInetAddress() == null)
+		if(network
+				.getModel()
+				.getInetAddress() == null)
 			throw new AssertionError("Received server address");
 	}
 
 	@Test public void getConfiguration()
 	throws IOException, InterruptedException {
-		MenuModel config = NetworkController.getInstance()
-											.getModel()
-											.getConfiguration();
+		MenuModel config = network
+				.getModel()
+				.getConfiguration();
 
 		if(!MenuController.getInstance().getModel().equals(config))
 			throw new AssertionError("Received configuration");
 	}
 
 	@Test public void getServerPort() {
-		if(NetworkController.getInstance()
-							.getModel()
-							.getPort() != 3333)
+		if(network
+				.getModel()
+				.getPort() != 3333)
 			throw new AssertionError("Received server port");
 	}
 
 	@Test public void getNumberOfPlayers() {
-		if(NetworkController.getInstance()
-							.getModel()
-							.getNumberOfPlayers() != 1)
+		if(network
+				.getModel()
+				.getNumberOfPlayers() != 1)
 			throw new AssertionError("Received number of players");
 	}
 }
